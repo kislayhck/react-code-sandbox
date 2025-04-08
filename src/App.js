41e1909aa8738f86b049d65d7d5d7e8375712694
Kislay Kislay <kislay.parallel@gmail.com>
2025-04-08 06:38:13 +0000

    added common dropdown component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import "./styles.css";
 import { useEffect, useState } from "react";
-import Accordion from "./components/Accordion"
-import ProgressBar from "./components/ProgressBar"
-import DataGrid from "./components/DataGrid"
-import Modal from "./components/Modal"
-import ImgCaraousel from "./components/ImgCaraousel"
-import TransferList from "./components/TransferList"
-import users from './data/users.json';
-import images from './data/images.json';
+import Accordion from "./components/Accordion";
+import ProgressBar from "./components/ProgressBar";
+import DataGrid from "./components/DataGrid";
+import Modal from "./components/Modal";
+import ImgCaraousel from "./components/ImgCaraousel";
+import TransferList from "./components/TransferList";
+import DropDown from "./components/FormFields/DropDown";
+import users from "./data/users.json";
+import images from "./data/images.json";
 
 export default function App() {
-  const [isOpen,setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [selectedValue, setSelectedValue] = useState("Option 1");
+  const options = ["Option 1", "Option 2", "Option 3"];
+
+  const handleInputChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-        {/* <Accordion  />
+      <h1>Hello CodeSandbox {selectedValue}</h1>
+      {/* <Accordion  />
         <ProgressBar /> */}
-        {/* <DataGrid users={users}/>
+      {/* <DataGrid users={users}/>
         <Modal 
           isOpen={isOpen} 
           onClose={() => setIsOpen(false)} 
@@ -25,8 +33,12 @@ export default function App() {
           children={"One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections."}
         />
         <button onClick={() => setIsOpen(true)}>Show modal</button> */}
-        {/* <ImgCaraousel images={images}/> */}
-        <TransferList />
+      {/* <ImgCaraousel images={images}/> */}
+      <DropDown
+        options={options}
+        selectedValue={selectedValue}
+        handleChange={handleInputChange}
+      />
     </div>
   );
 }
